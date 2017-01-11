183e203aa159c038ef175c449ebad1806c33b800
Laurent Berthier <proberthier@gmail.com>
2017-01-11 10:46:42 +0100

    trying to sync with googleapiclient

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -19,6 +19,7 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.text.format.DateUtils;
+import android.util.Log;
 
 import com.example.android.sunshine.data.SunshinePreferences;
 import com.example.android.sunshine.data.WeatherContract;
@@ -75,6 +76,9 @@ public class SunshineSyncTask {
                 sunshineContentResolver.bulkInsert(
                         WeatherContract.WeatherEntry.CONTENT_URI,
                         weatherValues);
+
+                Log.d("SunshineSyncTask", "1");
+                SunshineSyncUtils.updateWatchFace(context);
 
                 /*
                  * Finally, after we insert data into the ContentProvider, determine whether or not
